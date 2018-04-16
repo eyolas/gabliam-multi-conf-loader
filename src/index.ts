8f25bbdda2d9dd05a36f8d946d55aafa602ef8ae
eyolas <dtouzet@gmail.com>
2018-04-16 16:48:15 +0200

    feat: Add resolver for congif

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as fs from 'fs';
 import * as _ from 'lodash';
 import * as d from 'debug';
 import { promisify, inspect } from 'util';
+import { configResolver, Resolver } from '@gabliam/core';
 
 // Promisify
 const glob = promisify(require('glob')); // require and no import for typings bug
@@ -13,6 +14,7 @@ const debug = d('gabliam-multi-conf-loader:FileLoader');
 async function loadConstant(
   folder: string,
   projectName: string,
+  resolver: Resolver,
   profile?: string
 ) {
   const files: string[] = await glob(`**/constants?(-+([a-zA-Z])).@(yml)`, {
@@ -30,7 +32,7 @@ async function loadConstant(
     constants = _.merge(
       {},
       constants,
-      await loadFile(`${folder}/${defaultProfileFile}`)
+      await loadFile(`${folder}/${defaultProfileFile}`, resolver)
     );
   }
 
@@ -41,7 +43,7 @@ async function loadConstant(
       constants = _.merge(
         {},
         constants,
-        await loadFile(`${folder}/${profileFile}`)
+        await loadFile(`${folder}/${profileFile}`, resolver)
       );
     }
   }
@@ -56,7 +58,8 @@ export default async function FileLoader(
   profile?: string
 ) {
   debug('loadConfig', folder);
-  const constants = await loadConstant(folder, projectName, profile);
+  const resolver = configResolver(folder);
+  const constants = await loadConstant(folder, projectName, resolver, profile);
 
   // create tag config
   const ConfigYamlType = new yaml.Type('!config', {
@@ -87,7 +90,8 @@ export default async function FileLoader(
       await loadConfigProject(
         `${folder}/${defaultProfileFile}`,
         projectName,
-        VGO_SCHEMA
+        VGO_SCHEMA,
+        resolver
       )
     );
   }
@@ -104,7 +108,8 @@ export default async function FileLoader(
         await loadConfigProject(
           `${folder}/${profileFile}`,
           projectName,
-          VGO_SCHEMA
+          VGO_SCHEMA,
+          resolver
         )
       );
     }
@@ -116,21 +121,22 @@ export default async function FileLoader(
 async function loadConfigProject(
   filePath: string,
   projectName: string,
-  schema: yaml.Schema
+  schema: yaml.Schema,
+  resolver: Resolver
 ) {
   const data = await readFile(filePath, 'utf8');
   try {
     const vals = yaml.loadAll(data, undefined, { schema }) || [];
-    return _.find(vals, { projectName }) || {};
+    return await resolver(_.find(vals, { projectName }) || {});
   } catch (e) {
     return {};
   }
 }
 
-async function loadFile(filePath: string): Promise<Object> {
+async function loadFile(filePath: string, resolver: Resolver): Promise<Object> {
   const data = await readFile(filePath, 'utf8');
   try {
-    return yaml.load(data) || {};
+    return await resolver(yaml.load(data) || {});
   } catch (e) {
     return {};
   }
